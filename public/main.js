bca36aa689103fd0aa16eaa1631a99f5bf5b1969
SatwikVats <vats.satwik@gmail.com>
2024-01-31 01:09:23 +0530

    Updated the Table in public HTML

--- public/main.js
+++ public/main.js
@@ -10,17 +10,35 @@ socket.on('active-clients', (data) => {
 
 socket.on('messages', (data) => {
     console.log('Message', data);
-    const newRow = tableBody.insertRow(tableBody.ariaRowSpan.length);
 
-    for(let i=0;i<5;i++){
-        cells[i] = newRow.insertCell(i);
-    }
-
-    const dummy_val = "abc";
-
-    cells[0].innerHTML = `<input type="text" name="${dummy_val}">`;
-    cells[1].innerHTML = `<input type="text" name="${dummy_val}">`;
-    cells[2].innerHTML = `<input type="text" name="${dummy_val}">`;
-    cells[3].innerHTML = `<input type="text" name="${dummy_val}">`;
-    cells[4].innerHTML = `<input type="text" name="${dummy_val}">`;
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+    <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black; padding: 10px;">${data.senderId}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.chatId}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.senderFirstName}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.senderLastName}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.chatType}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.chatName}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.sentAt}</td>
+        <td style="border: 1px solid black; padding: 10px;">${data.text}</td>
+    </tr>
+    `
+    
+    tableBody.appendChild(newRow);
 })
+
+
+
+
+
+
+
+// const newRow = tableBody.insertRow(tableBody.ariaRowSpan.length);
+    // const newRow = tableBody.insertRow(tableBody);
+
+    // for(let i=0;i<8;i++){
+    //     cells[i] = newRow.insertCell(i);
+    // }
+
+    // const dummy_val = "abc";
