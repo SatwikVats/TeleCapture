ac348a47e73bf7fb11e50460c7f5b283be10043a
SatwikVats <vats.satwik@gmail.com>
2024-01-31 16:57:33 +0530

    Updated exception handling incase of Redis connection error.

--- config/redisConnector.ts
+++ config/redisConnector.ts
@@ -5,27 +5,33 @@ dotenv.config();
 
 const connectRedis = async () => {
 
-    const isDocker = process.env.DOCKER === 'true'? true: false;
-    let client;
+    try{
+        const isDocker = process.env.DOCKER === 'true'? true: false;
+        let client;
 
-    if(isDocker){
-        //Updating the host incase server is running inside a Docker conatiner.
-        client = createClient({url: "redis://@redis-telecapture:6379"});
+        if(isDocker){
+            //Updating the host incase server is running inside a Docker conatiner.
+            client = createClient({url: "redis://@redis-telecapture:6379"});
+        }
+        else{
+            // Default host 127.0.0.1 and port 6379 otherwise.
+            client = createClient();
+        }
+
+        client.on('end', () => {
+        console.log('Connection to Redis closed');
+        });
+
+        await client.connect();
+        return client;
+        
     }
-    else{
-        // Default host 127.0.0.1 and port 6379 otherwise.
-        client = createClient();
+    catch(err){
+        console.error(err);
+        throw new Error("Failed to establish a redis client connection!");
     }
 
-    client.on('error', (error: any) => {
-    console.error('Redis connection error:', error);
-    });
-    client.on('end', () => {
-    console.log('Connection to Redis closed');
-    });
-
-    await client.connect();
-    return client;
+    
 }
 
 export default connectRedis;
