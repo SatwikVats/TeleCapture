89df727033bbc70870b8b789bf9160bcdd433394
SatwikVats <vats.satwik@gmail.com>
2024-01-31 23:59:51 +0530

    Made a few fields optional to avoid schema validation failures.

--- model/message.model.ts
+++ model/message.model.ts
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema({
     senderId: {type: String, required: true},
     chatId: {type: String, required: true},
-    senderFirstName: {type: String, required: true},
-    senderLastName: {type: String, required: true},
+    senderFirstName: {type: String, required: false},
+    senderLastName: {type: String, required: false},
     chatName: {type: String},
     // message: {type: Object, required: true},
-    text: {type: String, required: true},
-    chatType: {type: String, required: true},
+    text: {type: String, required: false},
+    chatType: {type: String, required: false},
     sentAt: {type: String, required: true},
 },{
     timestamps: true,
